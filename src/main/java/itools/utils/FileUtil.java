01393a017cf8cc1333c603a272925dbd3ec7e771
wuyechun2018 <wuyechun2018@163.com>
2016-10-24 17:11:30 +0800

    add the DbUtil and a maven plugin to get the jar

---
+++ src/main/java/itools/utils/FileUtil.java
@@ -0,0 +1,13 @@
+package itools.utils;
+/**
+ * 
+ * 文件处理
+ * @desc: itools
+ * @author: wyc
+ * @createTime: 2016年10月24日 下午5:01:52
+ * @history:
+ * @version: v1.0
+ */
+public class FileUtil {
+
+}
